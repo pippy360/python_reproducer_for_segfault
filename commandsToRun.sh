2d7575dd2b7e1c25c7edec048838095bf674cf35
Tom Murphy <murphyt7@tcd.ie>
2019-02-02 17:40:37 +0000

    Create commandsToRun.sh

---
+++ commandsToRun.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git -y
+git clone https://github.com/pippy360/python_reproducer_for_segfault.git
+cd python_reproducer_for_segfault/
+sudo apt-get install python3-pip -y
+pip3 install .
+pip3 install .
